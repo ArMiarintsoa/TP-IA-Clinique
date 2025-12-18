0907c1185893df59c54dffe5a9525cfc569fa389
Fl@vio <ramanantsoa1311@gmail.com>
2025-12-18 14:54:12 +0300

    add levenshtein

--- app/text/routers.py
+++ app/text/routers.py
@@ -4,6 +4,8 @@ from .service import TextService
 router = APIRouter(prefix="/text", tags=["text"])
 
 @router.get("/analyze")
-def analyse_text(text: str):
-    lev = TextService().levenshtein_distance(text, "textam")
-    return { "message": lev }
+def analyze_text(text: str):
+    csv_file = "../teny_malagasy.csv"
+    closest_word, distance = TextService.find_closest_word(text, csv_file)
+    print(f"Mot le plus proche : {closest_word}, Distance : {distance}")
+    return { "closest_word": closest_word }
