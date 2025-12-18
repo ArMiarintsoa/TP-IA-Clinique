1e5499b79880d14c6c12b09b613ea8f03cbf5430
Miarintsoa Itokiana <arimalalamiari@Gmail.com>
2025-12-18 16:00:06 +0300

    change cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from typing import Union
 from app.text.routers import router as text_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Text editor API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # autorise toutes les origines
+    allow_credentials=True,
+    allow_methods=["*"],        # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],        # tous les headers
+)
 
 app.include_router(text_router)
